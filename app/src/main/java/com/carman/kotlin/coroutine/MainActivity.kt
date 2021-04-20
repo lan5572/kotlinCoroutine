b14adafcf9c18bedcadd5159beb366bff3a92d0c
carman <feigebabata@outlook.com>
2021-04-20 21:11:19 +0800

    协同作用域

--- app/src/main/java/com/carman/kotlin/coroutine/MainActivity.kt
+++ app/src/main/java/com/carman/kotlin/coroutine/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.*
+import java.lang.NullPointerException
 import kotlin.coroutines.ContinuationInterceptor
 import kotlin.coroutines.CoroutineContext
 
@@ -21,7 +22,11 @@ class MainActivity : AppCompatActivity() {
 //            start()
 //            testCoroutineContext()
 //            testCoroutineStart()
-            testUnDispatched()
+//            testUnDispatched()
+//            testCoroutineScope()
+//            testCoroutineScope2()
+//            testCoroutineScope4()
+            testCoroutineScope4()
         }
     }
 
@@ -35,23 +40,23 @@ class MainActivity : AppCompatActivity() {
             btn.text = result
         }
 
-   /*     GlobalScope.launch(Dispatchers.Main) {
-            for (index in 1 until  10) {
-                //同步执行
-                launch {
-                    Log.d("launch$index", "启动一个协程")
-                }
-            }
-        }*/
+        /*     GlobalScope.launch(Dispatchers.Main) {
+                 for (index in 1 until  10) {
+                     //同步执行
+                     launch {
+                         Log.d("launch$index", "启动一个协程")
+                     }
+                 }
+             }*/
 
-       /* GlobalScope.launch {
-            for (index in 1 until  10) {
-                //并发执行
-                launch {
-                    Log.d("launch$index", "启动一个协程")
-                }
-            }
-        }*/
+        /* GlobalScope.launch {
+             for (index in 1 until  10) {
+                 //并发执行
+                 launch {
+                     Log.d("launch$index", "启动一个协程")
+                 }
+             }
+         }*/
 
 
         /*GlobalScope.launch{
@@ -67,19 +72,19 @@ class MainActivity : AppCompatActivity() {
             Log.d("asyncJob", "$asyncJob")
         }*/
 
-       /* val runBlockingJob = runBlocking {
-            Log.d("runBlocking", "启动一个协程")
-        }
-        Log.d("runBlockingJob", "$runBlockingJob")
-        val launchJob = GlobalScope.launch{
-            Log.d("launch", "启动一个协程")
-        }
-        Log.d("launchJob", "$launchJob")
-        val asyncJob = GlobalScope.async{
-            Log.d("async", "启动一个协程")
-            "我是返回值"
-        }
-        Log.d("asyncJob", "$asyncJob")*/
+        /* val runBlockingJob = runBlocking {
+             Log.d("runBlocking", "启动一个协程")
+         }
+         Log.d("runBlockingJob", "$runBlockingJob")
+         val launchJob = GlobalScope.launch{
+             Log.d("launch", "启动一个协程")
+         }
+         Log.d("launchJob", "$launchJob")
+         val asyncJob = GlobalScope.async{
+             Log.d("async", "启动一个协程")
+             "我是返回值"
+         }
+         Log.d("asyncJob", "$asyncJob")*/
 
 
 //        runBlocking {
@@ -93,26 +98,26 @@ class MainActivity : AppCompatActivity() {
 //        }
     }
 
-    private fun testCoroutineContext(){
+    private fun testCoroutineContext() {
         val coroutineContext1 = Job() + CoroutineName("这是第一个上下文")
         Log.d("coroutineContext1", "$coroutineContext1")
-        val  coroutineContext2 = coroutineContext1 + Dispatchers.Default + CoroutineName("这是第二个上下文")
+        val coroutineContext2 = coroutineContext1 + Dispatchers.Default + CoroutineName("这是第二个上下文")
         Log.d("coroutineContext2", "$coroutineContext2")
         val coroutineContext3 = coroutineContext2 + Dispatchers.Main + CoroutineName("这是第三个上下文")
         Log.d("coroutineContext3", "$coroutineContext3")
     }
 
-    private fun testCoroutineStart(){
-        val defaultJob = GlobalScope.launch{
+    private fun testCoroutineStart() {
+        val defaultJob = GlobalScope.launch {
             Log.d("defaultJob", "CoroutineStart.DEFAULT")
         }
         defaultJob.cancel()
 
-        val lazyJob = GlobalScope.launch(start = CoroutineStart.LAZY){
+        val lazyJob = GlobalScope.launch(start = CoroutineStart.LAZY) {
             Log.d("lazyJob", "CoroutineStart.LAZY")
         }
 
-        val atomicJob = GlobalScope.launch(start = CoroutineStart.ATOMIC){
+        val atomicJob = GlobalScope.launch(start = CoroutineStart.ATOMIC) {
             Log.d("atomicJob", "CoroutineStart.ATOMIC挂起前")
             delay(100)
             Log.d("atomicJob", "CoroutineStart.ATOMIC挂起后")
@@ -120,7 +125,7 @@ class MainActivity : AppCompatActivity() {
 
         atomicJob.cancel()
 
-        val undispatchedJob = GlobalScope.launch(start = CoroutineStart.UNDISPATCHED){
+        val undispatchedJob = GlobalScope.launch(start = CoroutineStart.UNDISPATCHED) {
             Log.d("undispatchedJob", "CoroutineStart.UNDISPATCHED挂起前")
             delay(100)
             Log.d("atomicJob", "CoroutineStart.UNDISPATCHED挂起后")
@@ -128,16 +133,109 @@ class MainActivity : AppCompatActivity() {
         undispatchedJob.cancel()
     }
 
-    private fun testUnDispatched(){
-        GlobalScope.launch(Dispatchers.Main){
-           val job = launch(start = CoroutineStart.UNDISPATCHED) {
-               Log.d("${Thread.currentThread().name}线程", "-> 挂起前")
-               delay(100)
-               Log.d("${Thread.currentThread().name}线程", "-> 挂起后")
-           }
-           Log.d("${Thread.currentThread().name}线程", "-> join前")
-           job.join()
-           Log.d("${Thread.currentThread().name}线程", "-> join后")
-       }
+    private fun testUnDispatched() {
+        GlobalScope.launch(Dispatchers.Main) {
+            val job = launch(start = CoroutineStart.UNDISPATCHED) {
+                Log.d("${Thread.currentThread().name}线程", "-> 挂起前")
+                delay(100)
+                Log.d("${Thread.currentThread().name}线程", "-> 挂起后")
+            }
+            Log.d("${Thread.currentThread().name}线程", "-> join前")
+            job.join()
+            Log.d("${Thread.currentThread().name}线程", "-> join后")
+        }
+    }
+
+    private fun testCoroutineScope() {
+        GlobalScope.launch(Dispatchers.Main) {
+            Log.d("父协程上下文", "$coroutineContext")
+            launch(CoroutineName("第一个子协程")) {
+                Log.d("第一个子协程上下文", "$coroutineContext")
+            }
+            launch(Dispatchers.Unconfined) {
+                Log.d("第二个子协程协程上下文", "$coroutineContext")
+            }
+        }
+    }
+
+
+    private fun testCoroutineScope2() {
+        val exceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+            Log.d("exceptionHandler", "${coroutineContext[CoroutineName]} $throwable")
+        }
+        GlobalScope.launch(Dispatchers.Main + CoroutineName("scope1") + exceptionHandler) {
+            Log.d("scope", "--------- 1")
+            launch(CoroutineName("scope2") + exceptionHandler) {
+                Log.d("scope", "--------- 2")
+                throw  NullPointerException("空指针")
+                Log.d("scope", "--------- 3")
+            }
+            val scope3 = launch(CoroutineName("scope3") + exceptionHandler) {
+                Log.d("scope", "--------- 4")
+                delay(2000)
+                Log.d("scope", "--------- 5")
+            }
+            scope3.join()
+            Log.d("scope", "--------- 6")
+        }
+    }
+
+
+    private fun testCoroutineScope3() {
+        val exceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+            Log.d("exceptionHandler", "${coroutineContext[CoroutineName]} $throwable")
+        }
+        GlobalScope.launch(Dispatchers.Main + CoroutineName("scope1") + exceptionHandler) {
+            supervisorScope {
+                Log.d("scope", "--------- 1")
+                launch(CoroutineName("scope2") + exceptionHandler) {
+                    Log.d("scope", "--------- 2")
+                    throw  NullPointerException("空指针")
+                    Log.d("scope", "--------- 3")
+                    val scope3 = launch(CoroutineName("scope3") + exceptionHandler) {
+                        Log.d("scope", "--------- 4")
+                        delay(2000)
+                        Log.d("scope", "--------- 5")
+                    }
+                    scope3.join()
+                }
+                val scope4 = launch(CoroutineName("scope4") + exceptionHandler) {
+                    Log.d("scope", "--------- 6")
+                    delay(2000)
+                    Log.d("scope", "--------- 7")
+                }
+                scope4.join()
+                Log.d("scope", "--------- 8")
+            }
+        }
+    }
+
+    private fun testCoroutineScope4() {
+        val exceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+            Log.d("exceptionHandler", "${coroutineContext[CoroutineName]} $throwable")
+        }
+        GlobalScope.launch(Dispatchers.Main + CoroutineName("scope1") + exceptionHandler) {
+            supervisorScope {
+                Log.d("scope", "--------- 1")
+                launch(CoroutineName("scope2") + exceptionHandler) {
+                    Log.d("scope", "--------- 2")
+                    throw  NullPointerException("空指针")
+                    Log.d("scope", "--------- 3")
+                    val scope3 = launch(CoroutineName("scope3") + exceptionHandler) {
+                        Log.d("scope", "--------- 4")
+                        delay(2000)
+                        Log.d("scope", "--------- 5")
+                    }
+                    scope3.join()
+                }
+                val scope4 = launch(CoroutineName("scope4") + exceptionHandler) {
+                    Log.d("scope", "--------- 6")
+                    delay(2000)
+                    Log.d("scope", "--------- 7")
+                }
+                scope4.join()
+                Log.d("scope", "--------- 8")
+            }
+        }
     }
 }
